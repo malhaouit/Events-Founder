4bc3910e4750a097b4470f294f16a2db77565987
malhaouit <abdelmounaim.abdel@gmail.com>
2024-06-11 07:33:16 -0700

    Comment and clean the code

--- classes/models/Event.java
+++ classes/models/Event.java
@@ -3,6 +3,7 @@ package com.amtrustdev.localeventfinder.models;
 import com.google.firebase.firestore.Exclude;
 import com.google.firebase.firestore.PropertyName;
 
+// The Event class that represents each created or founded event
 public class Event {
     private String eventName;
     private String dateTime;
@@ -11,7 +12,7 @@ public class Event {
     private String category;
     private String locationId;
     @Exclude
-    private String documentId; // Use @Exclude to avoid serialization
+    private String documentId; // @Exclude notation is used to avoid serialization of the document ID
     private boolean isOnline;
     private String userId;
 
@@ -19,10 +20,9 @@ public class Event {
     public Event() {
     }
 
-    // Parameterized constructor
-
-
-    public Event(String eventName, String dateTime, String description, String details, String category, String locationId, boolean isOnline, String userId) {
+    // Event constructor
+    public Event(String userId, String eventName, String dateTime, String description, String details, String category, String locationId, boolean isOnline) {
+        this.userId = userId;
         this.eventName = eventName;
         this.dateTime = dateTime;
         this.description = description;
@@ -30,81 +30,98 @@ public class Event {
         this.category = category;
         this.locationId = locationId;
         this.isOnline = isOnline;
+    }
+
+    // Get user ID
+    public String getUserId() {
+        return userId;
+    }
+
+    // Set user ID
+    public void setUserId(String userId) {
         this.userId = userId;
     }
 
+    // Get event name
     public String getEventName() {
         return eventName;
     }
 
+    // Set event name
     public void setEventName(String eventName) {
         this.eventName = eventName;
     }
 
+    // Get event datetime
     public String getDateTime() {
         return dateTime;
     }
 
+    // Set event datetime
     public void setDateTime(String dateTime) {
         this.dateTime = dateTime;
     }
 
+    // Get event description
     public String getDescription() {
         return description;
     }
 
+    // Sett event description
     public void setDescription(String description) {
         this.description = description;
     }
 
+    // Get event details
     public String getDetails() {
         return details;
     }
 
+    // Set event details
     public void setDetails(String details) {
         this.details = details;
     }
 
+    // Get event category
     public String getCategory() {
         return category;
     }
 
+    // Set event category
     public void setCategory(String category) {
         this.category = category;
     }
 
+    // Get location ID
     public String getLocationId() {
         return locationId;
     }
 
+    // Set location ID
     public void setLocationId(String locationId) {
         this.locationId = locationId;
     }
 
+    // Get Firestore document ID
     @Exclude
     public String getDocumentId() {
         return documentId;
     }
 
+    // Set Firestore document ID
     public void setDocumentId(String documentId) {
         this.documentId = documentId;
     }
 
+    // Get boolean value of isOnline field depending on the event is Online or not
     @PropertyName("isOnline")
     public boolean isOnline() {
         return isOnline;
     }
 
+    // Set boolean value to isOnline field depending on the event is Online or not
     @PropertyName("isOnline")
     public void setOnline(boolean online) {
         isOnline = online;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
     }
 }
