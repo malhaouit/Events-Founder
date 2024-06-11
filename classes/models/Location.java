285fc4b287a852d3333d11c8f687b4501483a11c
malhaouit <abdelmounaim.abdel@gmail.com>
2024-06-11 07:41:48 -0700

    Comment and clean the code

--- classes/models/Location.java
+++ classes/models/Location.java
@@ -2,62 +2,59 @@ package com.amtrustdev.localeventfinder.models;
 
 import com.google.firebase.firestore.Exclude;
 
+// Location class responsible for data modeling of each event's location
 public class Location {
     @Exclude
-    private String locationId;
+    private String locationId; // @Exclude notation is used here to avoid serialization of the location ID
     private String street;
     private String city;
     private String country;
 
-    // Default constructor (required for Firebase)
-    public Location() {
-    }
-
-    // Parameterized constructor
-    public Location(String locationId, String street, String city, String country) {
-        this.locationId = locationId;
-        this.street = street;
-        this.city = city;
-        this.country = country;
-    }
-
+    // Location constructor for initializing location fields
     public Location(String street, String city, String country) {
         this.street = street;
         this.city = city;
         this.country = country;
     }
-
-    // Getters and setters
+    
+    // Get location ID
+    @Exclude
     public String getLocationId() {
         return locationId;
     }
 
+    // Set location ID
     public void setLocationId(String locationId) {
         this.locationId = locationId;
     }
 
+    // Get location street
     public String getStreet() {
         return street;
     }
 
+    // Set location street
     public void setStreet(String street) {
         this.street = street;
     }
 
+    // Get location city
     public String getCity() {
         return city;
     }
-
+    
+    // Set location city
     public void setCity(String city) {
         this.city = city;
     }
 
+    // Get location country
     public String getCountry() {
         return country;
     }
 
+    // Set location country
     public void setCountry(String country) {
         this.country = country;
     }
 }
-
